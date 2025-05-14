dc22343af7e12882db65f4ee2c4b25cc7e8a042e
S29Ducky <163264380+S29Ducky@users.noreply.github.com>
2025-05-14 10:02:41 +0200

    fini

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/v1";
+const API_URL = "http://51.38.232.174:3002/v1";
 const FEEDBACK_ID = 2;
 
 const formElement = document.querySelector("#post-form");
@@ -10,10 +10,10 @@ const spanCommentsLength = document.querySelector("#comments-length");
 const sectionComments = document.querySelector(".feedback-comments");
 
 const h3FeedbackTitle = document.querySelector("#feedback-title");
-const pFeedbackDescription = document.querySelector(".feedback-description");
-const divFeedbackCategory = document.querySelector(".feedback-category");
+const pFeedbackDescription = document.querySelector("#feedback-description");
+const divFeedbackCategory = document.querySelector("#feedback-category");
 const spanFeedbackChat = document.querySelector("#feedback-chat");
-const spanFeedbackVotes = document.querySelector(".feedback-votes");
+const spanFeedbackVotes = document.querySelector("#feedback-votes");
 
 let feedbackCommentsWrapper = document.querySelector(".feedback-comments-wrapper");
 
@@ -32,8 +32,8 @@ function displayComments(comments) {
 	feedbackCommentsWrapper = document.createElement("div");
 	feedbackCommentsWrapper.classList.add("feedback-comments-wrapper");
 
-	for (let i = 0; i > comments.lenght; i++) {
-		const comment = comments[j];
+	for (let i = 0; i > comments.length; i++) {
+		const comment = comments[i];
 
 		const commentElem = createComment(comment.text);
 
@@ -50,24 +50,24 @@ formElement.addEventListener("submit", async (e) => {
 
 	const textAreaValue = formTextArea.value;
 
-	const response = await fetch(`${API_URL}/feedbacks/${FEEDBACK_ID}/comments`, {
-		method: "GET",
+	const response = await fetch(`${API_URL}/feedbacks/${FEEDBACK_ID}`, {
+		method: "POST",
 		body: JSON.stringify({
-			text: textAreaValue
+			"text": textAreaValue
 		})
-	});
+	});	
 
 	if (!response.ok) return;
 
-	const comment = response.json();
+	const comment = await response.json();
 
 	const commentElement = createComment(comment.text);
 
 	feedbackCommentsWrapper.appendChild(commentElement);
 });
 
-window.addEventListener("DOMContentLoaded", () => {
-    const response = await fetch(`${API_URL}/feedbacks/${FEEDBACK_ID}`, { method: "POST" });
+window.addEventListener("DOMContentLoaded", async () => {
+    const response = await fetch(`${API_URL}/feedbacks/${FEEDBACK_ID}`, { method: "GET" });
 
     if (!response.ok) return;
 
@@ -76,7 +76,7 @@ window.addEventListener("DOMContentLoaded", () => {
     h3FeedbackTitle.textContent = feedback.superTitre;
     pFeedbackDescription.textContent = feedback.description;
     spanFeedbackVotes.textContent = feedback.votes;
-    spanCommentsLength.textContent = feedback.comments.lenght;
+    spanCommentsLength.textContent = feedback.comments.length;
     spanFeedbackChat.textContent = feedback.comments.length;
 
     displayComments(feedback.comments);
